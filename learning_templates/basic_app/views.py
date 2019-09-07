5adacdc20ec1c4c5e08fb676d96ad641685ed1d7
saipranith <saipranith68@gmail.com>
2019-09-07 13:14:35 +0530

    changed html files for mess

--- learning_templates/basic_app/views.py
+++ learning_templates/basic_app/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-
+import time
 
 # Create your views here.
 
 def index(request):
-    context_dict = {'text':'hello world', 'number':100}
+    context_dict = {'text':'hello world', 'number':100, 'time':time.ctime()}
     return render(request, 'basic_app/index.html', context_dict)
 
 
